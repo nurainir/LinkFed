70528c03f362b312160009dac6f6a9139ea2f78b
Nur Aini Rakhmawati <nurainir@gmail.com>
2012-01-24 10:48:34 +0000

    added linkstat.sh

---
+++ Readme.md
@@ -0,0 +1,12 @@
+LinkStat
+--------
+
+Counting the number of link in heterogeneous dataset
+
+Coming soon
+-----------
+
+* Link Richness
+* Link redundancy and ambiguity 
+
+Stay tune :)
---
+++ linkstat.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#generate the number of links that are connected to other dataset
+#author	: Nur Aini
+#date	: January 24, 2012
+#Usage	:	chmod +x linkstat.sh
+#		./linkstat.sh ntriplesfile	
+
+grep -E -v 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type|\"|http://www.w3.org/2002/07/owl#equivalentclass|http://www.w3.org/2002/07/owl#equivalentProperty|http://www.w3.org/2000/01/rdf-schema#subClassOf' $1 | 
+awk '{
+ s=$1; p=$2; o=$3
+one=match(s, /<(.*)#/, subj)
+if(one==0)
+one=match(s, /<(.*)\/*>$/, subj) 
+subvalue=subj[1]
+if(one!=0)
+one=match(subj[1], /(.*)resource/, tes) 
+if(one!=0)
+subvalue=tes[1]
+
+one=match(o, /<(.*)#/, obj)
+if(one==0)
+one=match(o, /<(.*)\/*>$/, obj) 
+#print obj[1]
+objvalue=obj[1]
+if(one!=0)
+one=match(obj[1], /(.*)resource/, tes) 
+if(one!=0)
+objvalue=tes[1]
+
+  if (subvalue != objvalue) arr[p]++
+
+}
+END { 
+total =0
+for(no in arr) { 
+print arr[no], no 
+total=total+arr[no]
+}
+print total
+ }' 
