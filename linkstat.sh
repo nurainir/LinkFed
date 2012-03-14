6de292630d7a425fc7c54fa3ad67c4305736fac4
Nur Aini Rakhmawati <nurainir@gmail.com>
2012-03-14 14:43:47 +0000

    added link cost

--- linkstat.sh
+++ linkstat.sh
@@ -6,7 +6,7 @@
 #Usage	:	chmod +x linkstat.sh
 #		./linkstat.sh ntriplesfile	
 
-skipproperties='\"|http://www.w3.org/2002/07/owl#equivalentclass|http://www.w3.org/2002/07/owl#equivalentProperty|http://www.w3.org/2000/01/rdf-schema#subClassOf|^_|http://www.w3.org/2000/01/rdf-schema#Class|http://www.w3.org/1999/02/22-rdf-syntax-ns#Property'
+skipproperties='\"|http://www.w3.org/2002/07/owl#equivalentclass|http://www.w3.org/2002/07/owl#equivalentProperty|http://www.w3.org/2000/01/rdf-schema#subClassOf|^_|http://www.w3.org/2000/01/rdf-schema#Class|http://www.w3.org/1999/02/22-rdf-syntax-ns#Property|http://xmlns.com/foaf/0.1/page'
 
 type="http://www.w3.org/1999/02/22-rdf-syntax-ns#type"
 
@@ -22,8 +22,8 @@ o=substr($3, 1, length($3)-4)
 
 if ( match($2,"http://www.w3.org/1999/02/22-rdf-syntax-ns#type")!=0)
 {
-	SubEntity[s]=o
-	#print s,o
+	SubinClass[s]=o
+	
 }
 else if ( $3 !~ /.*\.(pdf|html|asp|php|jpg)/ ) 
 {
@@ -50,25 +50,27 @@ else if ( $3 !~ /.*\.(pdf|html|asp|php|jpg)/ )
 	
 	if(match(o, "http://(.*)#", obj)!=0 || match(o, /http:\/\/(.*):/, obj) !=0)
 	{
-		objvalue=obj[1]
-		
+		objvalue=obj[1]		
 	}
+
 	else (match(o, "http://(.*)/.*", obj) !=0)
 	{
-		
 		if(match(obj[1], /(.*)(resource|inserts)/, tes) !=0)
-		objvalue=tes[1]
+			objvalue=tes[1]
 		else
-		objvalue=obj[1]
+			objvalue=obj[1]
 	}
 
 	  if (subvalue != objvalue) 
 		{
 	
-		page = match(p, "*[/#](.*)$", predicate) 
-		if(match(predicate[1],"([Pp]age|link)")==0 )
+				
+		if(match(p,".*[#/].*([Pp]age|[Ll]ink).*$")==0 )
 		{	
 			arr[p]++
+			#drugbank issue
+			if(match(objvalue,"129.128.185.122/drugbank2/*"))
+				objvalue="129.128.185.122/drugbank2"
 			SubLink[s,p,objvalue]++
 			otherdataset[objvalue]++
 			
@@ -98,25 +100,30 @@ for(idother in otherdataset) {
 print total
 delete otherdataset
 print "--------------"
-print "Number of Link each Entity"
-print "Entity	Link	OtherDataset	TotalLink	Distinct Subject	Average"
+print "Number of Link each Class"
+print "Class	Link	OtherDataset	TotalLink	Distinct Subject	Average"
 
 for(isublink in SubLink)
 	{	
 		split(isublink, idsub, SUBSEP);
-		#print idsub[1],idsub[2],idsub[3],SubLink[isublink],SubEntity[idsub[1]]
-		EntityLink[SubEntity[idsub[1]],idsub[2],idsub[3]]=EntityLink[SubEntity[idsub[1]],idsub[2],idsub[3]]+SubLink[isublink]
+		ClassLink[SubinClass[idsub[1]],idsub[2],idsub[3]]=ClassLink[SubinClass[idsub[1]],idsub[2],idsub[3]]+SubLink[isublink]
 	}
 
-#calculate distinct subject each entity
-for (idSub in SubEntity)
-	EntitySub[SubEntity[idSub]]++
+#calculate distinct subject each class
+for (idSub in SubinClass)
+	EntitySub[SubinClass[idSub]]++
 
-for (ientitylink in EntityLink)
+cost_p=0
+cost_c=0
+for (iClassLink in ClassLink)
 {
-	split(ientitylink, identity, SUBSEP)
-	average=EntityLink[ientitylink]/EntitySub[identity[1]]	
-	print identity[1],identity[2],identity[3],EntityLink[ientitylink],EntitySub[identity[1]],average
+	split(iClassLink, identity, SUBSEP)
+	cost_p = cost_p + EntitySub[identity[1]]
+	cost_c++
+	average=ClassLink[iClassLink]/EntitySub[identity[1]]	
+	print identity[1],identity[2],identity[3],ClassLink[iClassLink],EntitySub[identity[1]],average
 }
-
+print "cost publisher " cost_p
+print "cost consumer " cost_c
+print "total cost " cost_p+cost_c
  }' 
